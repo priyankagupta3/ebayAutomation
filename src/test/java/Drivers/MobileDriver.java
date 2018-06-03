02095117d8706ba9c8ae27bf9ad89a55b534d23e
Apple <apple@Apples-MacBook-Pro.local>
2018-06-03 12:19:53 +0530

    final  commit

--- src/test/java/Drivers/MobileDriver.java
+++ src/test/java/Drivers/MobileDriver.java
@@ -1,23 +1,12 @@
 package com.ebay.Driver;
 
-import ObjectRepository.Login;
-import ObjectRepository.Search;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
 
 public class MobileDriver {
 
@@ -34,7 +23,7 @@ public class MobileDriver {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "6.0.1");
         capabilities.setCapability(MobileCapabilityType.APP, appPath.getAbsolutePath());
 
-        androidDriver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+        androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         Thread.sleep(5000);
 
 
